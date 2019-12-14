9391a8bf58fe4ca4bc44916e72689479260caf31
aayush-nts <aayush.bharadwaj@ntsindia.co.in>
2019-12-14 14:43:54 +0530

    Add provision API

--- NTSFoodCoupon/dynamoDB.py
+++ NTSFoodCoupon/dynamoDB.py
@@ -1,12 +1,19 @@
 import boto3
 import json
 
-ACCESS_KEY = "AKIAUOKXMQ67NFPZF6HC"
-SECRET_KEY = "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"
+ACCESS_KEY = ""
+SECRET_KEY = ""
 dynamo = boto3.resource('dynamodb',aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY,
                       region_name="us-east-1")
 
+
+def get_all_items(table):
+
+    table = dynamo.Table(table)
+
+    response = table.scan()
+    return response["Items"]
 
 def get_item(table, key):
     try:
@@ -35,10 +42,24 @@ get_key = {'Day':'Wednesday'}
 print get_item("NTSFoodCouponMenu", get_key)
 """
 
-
+'''
 # Test NTSUser
-# put_key = {"username":"aayush.bharadwaj@ntsindia.co.in","emp_id":225623,"date":"12-20-2019","user_string":"225623_12-20-2019","Breakfast":0,"Lunch":0,"Dinner":1,"rating":{"Breakfast":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"average":0,"Feedback":"0"},"Lunch":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"Feedback":"0","average":0},"Dinner":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"Feedback":"0","average":0}}}
-# put_item("NTSUser", put_key)
-get_key = {'user_string' : '223317'}
-print get_item("NTSUser", get_key)["tokens"]["Lunch"]==0
+put_key = {"username":"aayush.bharadwaj@ntsindia.co.in","emp_id":225623,"date":"12-14-2019","user_string":"225623_12-14-2019","Breakfast":0,"Lunch":1,"Dinner":1,"rating":{"Breakfast":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"average":0,"Feedback":"0"},"Lunch":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"Feedback":"0","average":0},"Dinner":{"Hygiene":-1,"FoodQuality":-1,"StaffBehaviour":-1,"Feedback":"0","average":0}}}
+put_item("NTSUser", put_key)
 
+get_key = {'Day' : 'Sunday'}
+print get_item("NTSFoodCouponMenu", get_key)
+
+
+# put_key = {"Date":"14-12-2019", "Breakfast":0,"Lunch":0,"Dinner":0}
+# put_item("NTSguest", put_key)
+
+
+# Test NTSUserList
+put_key = {"emp_id":"225623", "email":"aayush.bharadwaj@ntsindia.co.in", "shift":"nts-shift-5", "password":"****"}
+put_item("NTSUserList", put_key)
+get_key = {'emp_id':"225623"}
+print get_item("NTSUserList", get_key)
+'''
+
+# print get_all_items("NTSUserList")["Items"]
